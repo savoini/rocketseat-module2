77e9024cf2fc54781c2890547c62d0a81ec5997b
Gilberto Savoini <gilberto.savoini@gmail.com>
2019-04-28 14:42:09 -0300

    Adicionando mensagens flash para o usuário

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,11 @@ const guestMiddleware = require('./app/middlewares/guest')
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
-routes.use('/app', authMiddleware)
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success')
+  res.locals.flashError = req.flash('error')
+  return next()
+})
 
 routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
@@ -18,6 +22,7 @@ routes.post('/signin', SessionController.store)
 routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
+routes.use('/app', authMiddleware)
 routes.get('/app/dashboard', (req, res) => {
   console.log(req.session.user)
   res.render('dashboard')
